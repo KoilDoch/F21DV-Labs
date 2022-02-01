600535d3443552fa7e2da9fece3f75b0a4d70a07
kd41 <kyled073@gmail.com>
2022-02-01 22:39:28 +0000

    Exercise 10
    Using the csv file, sorts the ages of each patient into an array then uses the array to display the qauntity of heart failure patients by age

--- index.js
+++ index.js
@@ -86,8 +86,27 @@
 
 //--EXERCISE 10--
 let heartfailurecsv = 'https://raw.githubusercontent.com/akmand/datasets/master/heart_failure.csv';
-let over50;
+// array to hold the quantity of each age group
+let ages = [0,0,0,0];
 d3.csv(heartfailurecsv, (data) => {
-    over50 = data.age;
-   }).then(() => console.log(over50));
-   
+        // sort the ages into respective array index
+        if (data.age < 31) { ages[0]++; } else 
+        if (data.age < 41) { ages[1]++; } else 
+        if (data.age < 61) { ages[2]++; } else 
+        if (data.age < 101) { ages[3]++; }
+    }).then(() => createParagraphs());
+
+// function called to append to the paragraphs
+function createParagraphs() {
+    d3.select("body")
+        .selectAll("p")
+        .data(ages)
+        .join(
+            enter => { enter.append("p").append("text").text(d=>d); },
+            update => { update.append("text").text(d=>d); },
+            exit => { exit.remove(); }
+        );
+}
+
+
+    
