1f2957cb49a50e8b3b02a73df6732f6db1e8cc86
kd41 <kyled073@gmail.com>
2022-02-01 23:40:40 +0000

    Exercise 11
    Added an svg element which a box is drawn upon

--- index.js
+++ index.js
@@ -85,28 +85,49 @@
 // });
 
 //--EXERCISE 10--
-let heartfailurecsv = 'https://raw.githubusercontent.com/akmand/datasets/master/heart_failure.csv';
-// array to hold the quantity of each age group
-let ages = [0,0,0,0];
-d3.csv(heartfailurecsv, (data) => {
-        // sort the ages into respective array index
-        if (data.age < 31) { ages[0]++; } else 
-        if (data.age < 41) { ages[1]++; } else 
-        if (data.age < 61) { ages[2]++; } else 
-        if (data.age < 101) { ages[3]++; }
-    }).then(() => createParagraphs());
+// let heartfailurecsv = 'https://raw.githubusercontent.com/akmand/datasets/master/heart_failure.csv';
+// // array to hold the quantity of each age group
+// let ages = [0,0,0,0];
+// d3.csv(heartfailurecsv, (data) => {
+//         // sort the ages into respective array index
+//         if (data.age < 31) { ages[0]++; } else 
+//         if (data.age < 41) { ages[1]++; } else 
+//         if (data.age < 61) { ages[2]++; } else 
+//         if (data.age < 101) { ages[3]++; }
+//     }).then(() => createParagraphs());
 
-// function called to append to the paragraphs
-function createParagraphs() {
-    d3.select("body")
-        .selectAll("p")
-        .data(ages)
-        .join(
-            enter => { enter.append("p").append("text").text(d=>d); },
-            update => { update.append("text").text(d=>d); },
-            exit => { exit.remove(); }
-        );
-}
+// // function called to append to the paragraphs
+// function createParagraphs() {
+//     d3.select("body")
+//         .selectAll("p")
+//         .data(ages)
+//         .join(
+//             enter => { enter.append("p").append("text").text(d=>d); },
+//             update => { update.append("text").text(d=>d); },
+//             exit => { exit.remove(); }
+//         );
+// }
 
+//--Exercise 11--
+// line coordinates [x1,x2,y1,y2]
+var lines = [[100, 100, 100, 200], [100, 200, 200, 200], [200, 200, 200, 100], [200, 100, 100, 100]];
+// colors associated with each line
+var color = ['red', 'blue', 'green', 'purple'];
 
-    
+// Create SVG element
+var svg = d3.select('body')
+ .append('svg')
+ .attr('width', 400)
+ .attr('height', 400)
+ .style('border', '1px solid green')
+
+// add the lines
+svg.selectAll('line')
+    .data(lines)
+    .enter().append('line')
+        // for each index in the data, create a line
+        .attr('x1', (d=>d[0]))
+        .attr('x2', (d=>d[1]))
+        .attr('y1', (d=>d[2]))
+        .attr('y2', (d=>d[3]))
+        .attr('stroke', (d,i)=>color[i] );
