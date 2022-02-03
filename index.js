30b86f8342f3c7d2165b0ececcf18518bf16026e
kd41 <kyled073@gmail.com>
2022-02-03 16:50:10 +0000

    Exercise 19
    Bar graph is now created using a function meaning multiple files can be displayed at once

--- index.js
+++ index.js
@@ -318,58 +318,75 @@
 //     .attr("font-family", "sans-serif")
 //     .text(d=>d);
 
-//--EXERCISE 17 & 18--
-const data = [];
-
+//--EXERCISE 17 & 18 & 19--
 // graph info for display size
 const width = 500;
 const barHeight = 20;
 const margin = 1;
 
-// process the data
-d3.csv("csv/bar.csv", (d) => {
-    // parse to int with +
-    data.push(+d.population)
-}).then(() => createChart());
-
-function createChart() {
-    // set up the svg
-    var svg = d3.select("body")
+// returns a svg object
+const Svg = function(length) {
+    let svg = d3.select("body")
     .append("svg")
     .attr("width", width)
-    .attr("height", barHeight * data.length);
+    .attr("height", barHeight * length);
 
-    // create scale
-    var scale = d3.scaleLinear()
-    .domain([d3.min(data),d3.max(data)])
-    .range([50,500])
-
-    var g = svg.selectAll("g")
-    .data(data)
-    .enter()
-    .append("g")
-    .attr("transform", (d,i) => 
-        "translate(0," + i * barHeight +")");
-
-    // create the bars
-    g.append("rect")
-        .attr("width", d => scale(d))
-        .attr("fill", d => {
-            // color
-            if (d < 100)
-                return "green";
-            else if (d > 500)
-                return "red";
-            else
-                return "blue";
-        })
-        .attr("height", barHeight - margin );  
-        
-    // append text in bars    
-    g.append("text")
-        .attr("x", d => scale(d))
-        .attr("y", barHeight / 2)
-        .attr("dy", ".35em")
-        .style("text-anchor", "end")
-        .text(d => d);    
+    return svg;
 }
+
+// returns a new scale object
+const Scale = function(min, max) {
+    let scale = d3.scaleLinear()
+        .domain([min,max])
+        .range([50,500]);
+    
+    return scale;
+}
+
+function createChart(file) {
+    const data = [];
+    // process the data
+    d3.csv(file, (d) => {
+        // parse to int with +
+        data.push(+d.population)
+    }).then(() => {
+        // create svg and scale
+        let svg = Svg(data.length);
+        let scale = Scale(d3.min(data), d3.max(data));
+
+        // create new group
+        let g = svg.selectAll("g")
+            .data(data)
+            .enter()
+            .append("g")
+            .attr("transform", (d,i) => 
+                "translate(0," + i * barHeight +")");
+
+        // create the bars
+        g.append("rect")
+            .attr("width", d => scale(d))
+            .attr("fill", d => {
+                // color
+                if (d < 5000)
+                    return "green";
+                else if (d > 10000)
+                    return "red";
+                else
+                    return "blue";
+            })
+            .attr("height", barHeight - margin );  
+                
+        // append text in bars    
+        g.append("text")
+            .attr("x", d => scale(d))
+            .attr("y", barHeight / 2)
+            .attr("dy", ".35em")
+            .style("text-anchor", "end")
+            .text(d => d);   
+    });
+}
+
+// create first chart
+createChart("csv/bar1.csv");
+// create second chart
+createChart("csv/bar2.csv");
