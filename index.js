1a10821bb522f8a255bd5e5a0b184bc36b78f83f
kd41 <kyled073@gmail.com>
2022-02-04 02:15:48 +0000

    Exercise 28
    Color scaling added to bar chart

--- index.js
+++ index.js
@@ -475,222 +475,224 @@
 //     .call(x_axisTop);
 
 
-/***************/
-/* EXERCISE 21 */
-/***************/
-// const margin = {top: 20, right: 30, bottom: 40, left: 90, bar: 1};
-// const data = [50, 400, 300, 900, 250, 1000];
-// const width = 500 - margin.left - margin.right;
-// const barHeight = 20;
+/*******************/
+/* EXERCISE 21 & 28*/
+/*******************/
+const margin = {top: 20, right: 30, bottom: 40, left: 45, bar: 1};
+const data = [50, 400, 300, 900, 250, 55, 100, 205, 500 ,671, 1000];
+const width = 500 - margin.left - margin.right;
+const barHeight = 20;
+// colors associated with each bar
+var color = d3.scaleLinear().domain([1,data.length]).range(["blue","green"]);
 
-// var svg = d3.select("body")
-//     .append("svg")
-//     .attr("width", width + margin.left + margin.right)
-//     .attr("height", barHeight * data.length 
-//         + margin.top + margin.bottom)
-//     .attr("transform", 
-//         "translate(" + margin.left + ","+ margin.top+")");
+var svg = d3.select("body")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", barHeight * data.length 
+        + margin.top + margin.bottom)
+    .attr("transform", 
+        "translate(" + margin.left + ","+ margin.top+")");
 
-// // create the x scale
-// var xscale = d3.scaleLinear()
-//     .domain([0, d3.max(data)])
-//     .range([0, width]);
-// // create the x axis
-// var x_axis = d3.axisBottom()
-//     .scale(xscale);
-// // append x axis
-// svg.append("g")
-//     .attr("transform", "translate(" + margin.left + "," + 
-//         barHeight * data.length +")")
-//     .call(x_axis)
+// create the x scale
+var xscale = d3.scaleLinear()
+    .domain([0, d3.max(data)])
+    .range([0, width]);
+// create the x axis
+var x_axis = d3.axisBottom()
+    .scale(xscale);
+// append x axis
+svg.append("g")
+    .attr("transform", "translate(" + margin.left + "," + 
+        barHeight * data.length +")")
+    .call(x_axis)
 
-// // create the y scale using scaleband
-// // using scaleband instead of scaleLinear due to 
-// // the y axis being ordinal
-// var yscale = d3.scaleBand()
-//     .range([0, barHeight * data.length])
-//     // counting backwards down the list of elements
-//     .domain(data.map((d,i) => data.length - i));
+// create the y scale using scaleband
+// using scaleband instead of scaleLinear due to 
+// the y axis being ordinal
+var yscale = d3.scaleBand()
+    .range([0, barHeight * data.length])
+    // counting backwards down the list of elements
+    .domain(data.map((d,i) => data.length - i));
 
-// // create the y axis
-// var y_axis = d3.axisLeft()
-//     .scale(yscale);
+// create the y axis
+var y_axis = d3.axisLeft()
+    .scale(yscale);
 
-// // append y axis
-// svg.append("g")
-//     .attr("transform", "translate(" + margin.left + ",0)")
-//     .call(y_axis)
+// append y axis
+svg.append("g")
+    .attr("transform", "translate(" + margin.left + ",0)")
+    .call(y_axis)
     
-// // add the rectangle
-// svg.selectAll("rect")
-//     .data(data)
-//     .enter()
-//     .append("rect")
-//     // make sure they start on the 0
-//     .attr("x", xscale(0) + margin.left)
-//     .attr("y", (d,i) => i * barHeight)
-//     // scale is now based on the xscale
-//     .attr("width", d => xscale(d))
-//     .attr("height", barHeight)
-//     .attr("fill", "blue");
+// add the rectangle
+svg.selectAll("rect")
+    .data(data)
+    .enter()
+    .append("rect")
+    // make sure they start on the 0
+    .attr("x", xscale(0) + margin.left)
+    .attr("y", (d,i) => i * barHeight)
+    // scale is now based on the xscale
+    .attr("width", d => xscale(d))
+    .attr("height", barHeight)
+    .attr("fill", (d,i) => color(i));
 
 
 /****************************************/
 /* EXERCISE 22 & 23 & 24 & 25 & 26 & 27 */
 /****************************************/
-// Set Dimensions
-const xSize = 600;
-const ySize = 600;
-const margin = 40;
-const xMax = xSize - margin * 2;
-const yMax = ySize - margin * 2;
+// // Set Dimensions
+// const xSize = 600;
+// const ySize = 600;
+// const margin = 40;
+// const xMax = xSize - margin * 2;
+// const yMax = ySize - margin * 2;
 
-// Creates a new chart
-function chart() {
-    let obj = {}
-    let objdata = [];
-    let lines = [];
-    let color = ["blue", "green"];
-    let svg;
-    let x;
-    let y;
-    let xExtent;
-    let yExtent;
+// // Creates a new chart
+// function chart() {
+//     let obj = {}
+//     let objdata = [];
+//     let lines = [];
+//     let color = ["blue", "green"];
+//     let svg;
+//     let x;
+//     let y;
+//     let xExtent;
+//     let yExtent;
 
-    // set the data held in the chart
-    obj.setdata = (file) => {
-        d3.csv(file, (data) => {
-            objdata.push(data);
-        }).then(() => {
-            // Get the 'limits' of the data - the full extent 
-            // (mins and max) so the plotted data fits perfects
-            xExtent = d3.extent( objdata, d=> +d.x);
-            yExtent = d3.extent( objdata, d=> +d.y);
+//     // set the data held in the chart
+//     obj.setdata = (file) => {
+//         d3.csv(file, (data) => {
+//             objdata.push(data);
+//         }).then(() => {
+//             // Get the 'limits' of the data - the full extent 
+//             // (mins and max) so the plotted data fits perfects
+//             xExtent = d3.extent( objdata, d=> +d.x);
+//             yExtent = d3.extent( objdata, d=> +d.y);
 
-            // organise the object data into lines 
-            lines = d3.group(objdata, d => d.id);
+//             // organise the object data into lines 
+//             lines = d3.group(objdata, d => d.id);
 
-            // create the axis
-            obj.createAxis();
+//             // create the axis
+//             obj.createAxis();
 
-            // draw the lines
-            obj.drawLine();
-        })
-    }
+//             // draw the lines
+//             obj.drawLine();
+//         })
+//     }
 
-    // Append SVG object to the page
-    obj.createCanvas = () => {
-        svg = d3.select("body")
-            .append("svg")
-            .attr("width", xSize)
-            .attr("height", ySize)
-            .append("g")
-            .attr("transform", "translate(" + margin + "," +
-                margin + ")");
-    }
+//     // Append SVG object to the page
+//     obj.createCanvas = () => {
+//         svg = d3.select("body")
+//             .append("svg")
+//             .attr("width", xSize)
+//             .attr("height", ySize)
+//             .append("g")
+//             .attr("transform", "translate(" + margin + "," +
+//                 margin + ")");
+//     }
 
-    // create the axis
-    obj.createAxis = () => {
-        // X Axis
-        x = d3.scaleLinear()
-            .domain([xExtent[0], xExtent[1]])
-            .range([0, xMax]);
+//     // create the axis
+//     obj.createAxis = () => {
+//         // X Axis
+//         x = d3.scaleLinear()
+//             .domain([xExtent[0], xExtent[1]])
+//             .range([0, xMax]);
 
-        // bottom
-        svg.append("g")
-            .attr("transform", "translate(0," + yMax + ")")
-            .call(d3.axisBottom(x))
-            .attr("color", "green");    // make bottom axis green
+//         // bottom
+//         svg.append("g")
+//             .attr("transform", "translate(0," + yMax + ")")
+//             .call(d3.axisBottom(x))
+//             .attr("color", "green");    // make bottom axis green
 
-        // top
-        svg.append("g")
-            .call(d3.axisTop(x));
+//         // top
+//         svg.append("g")
+//             .call(d3.axisTop(x));
 
-        // Y Axis
-        y = d3.scaleLinear()
-            .domain([ yExtent[0], yExtent[1]])
-            .range([yMax,0]);
+//         // Y Axis
+//         y = d3.scaleLinear()
+//             .domain([ yExtent[0], yExtent[1]])
+//             .range([yMax,0]);
 
-        // left y axis
-        svg.append("g")
-            .call(d3.axisLeft(y));
+//         // left y axis
+//         svg.append("g")
+//             .call(d3.axisLeft(y));
 
-        // right y axis
-        svg.append("g")
-            .attr("transform", `translate(${yMax},0)`)
-            .call(d3.axisRight(y));
-    }
+//         // right y axis
+//         svg.append("g")
+//             .attr("transform", `translate(${yMax},0)`)
+//             .call(d3.axisRight(y));
+//     }
 
-    obj.drawLine = () => {
-        // Add the line
-        svg.selectAll(".line")
-            .data(lines)
-            .enter()
-            .append("g")
-            .attr("class", (d,i) => {return "line"+i;})
-            .append("path")
-                .attr("fill", "none")
-                .attr("stroke", (d,i) => color[i])
-                .attr("stroke-width", 1.5)
-                .attr("d", function (d) {
-                    return d3.line()
-                        .x((d) => { return x(d.x);})
-                        .y((d) => { return y(d.y)})
-                        (d[1]); // since key/value array, [1] is used to access the value
-                });
+//     obj.drawLine = () => {
+//         // Add the line
+//         svg.selectAll(".line")
+//             .data(lines)
+//             .enter()
+//             .append("g")
+//             .attr("class", (d,i) => {return "line"+i;})
+//             .append("path")
+//                 .attr("fill", "none")
+//                 .attr("stroke", (d,i) => color[i])
+//                 .attr("stroke-width", 1.5)
+//                 .attr("d", function (d) {
+//                     return d3.line()
+//                         .x((d) => { return x(d.x);})
+//                         .y((d) => { return y(d.y)})
+//                         (d[1]); // since key/value array, [1] is used to access the value
+//                 });
 
-        // group for the dots
-        var dotGroup = svg.selectAll("dot")
-                .data(lines)
-                .enter()
-                .append("g");
+//         // group for the dots
+//         var dotGroup = svg.selectAll("dot")
+//                 .data(lines)
+//                 .enter()
+//                 .append("g");
                
-        // append the circle dots
-        dotGroup.selectAll()
-                .data((d) => d[1])
-                .enter()
-                // filter for only line 0
-                .filter(d => d.id == 0)
-                .append("circle")
-                    .attr("class", "circle-marker")
-                    .attr("cx", function (d) { return x(d.x) } )
-                    .attr("cy", function (d) { return y(d.y) } )
-                    .attr("r", 2)
-                    .style("fill", "red")
+//         // append the circle dots
+//         dotGroup.selectAll()
+//                 .data((d) => d[1])
+//                 .enter()
+//                 // filter for only line 0
+//                 .filter(d => d.id == 0)
+//                 .append("circle")
+//                     .attr("class", "circle-marker")
+//                     .attr("cx", function (d) { return x(d.x) } )
+//                     .attr("cy", function (d) { return y(d.y) } )
+//                     .attr("r", 2)
+//                     .style("fill", "red")
         
-        // append the text
-        dotGroup.selectAll()
-            .data((d) => d[1])
-                .enter()
-                // filter for only line 0 and every 5 elements
-                .filter((d,i) => (d.id == 0) && (i % 5 == 0))
-                .append("text")
-                .text(d => `(x: ${d.x} , y: ${d.y})`)
-                .attr("x", function (d) { return x(d.x) } )
-                .attr("y", function (d) { return y(d.y) } )
-                .attr("font-size", "8px");
+//         // append the text
+//         dotGroup.selectAll()
+//             .data((d) => d[1])
+//                 .enter()
+//                 // filter for only line 0 and every 5 elements
+//                 .filter((d,i) => (d.id == 0) && (i % 5 == 0))
+//                 .append("text")
+//                 .text(d => `(x: ${d.x} , y: ${d.y})`)
+//                 .attr("x", function (d) { return x(d.x) } )
+//                 .attr("y", function (d) { return y(d.y) } )
+//                 .attr("font-size", "8px");
                 
 
-        // append the triangle dots
-        dotGroup.selectAll()
-                .data((d) => d[1])
-                .enter()
-                // filter only for line 1
-                .filter(d => d.id == 1)
-                .append("path")
-                    // add the triangle symbol
-                    .attr("d", d3.symbol()
-                            .type(d3.symbolTriangle)
-                            .size(15))
-                    .attr("transform", function (d) { 
-                        return `translate(${x(d.x)},${y(d.y)})`})
-                    .style("fill", "red");
-    }
-    return obj;
-}
+//         // append the triangle dots
+//         dotGroup.selectAll()
+//                 .data((d) => d[1])
+//                 .enter()
+//                 // filter only for line 1
+//                 .filter(d => d.id == 1)
+//                 .append("path")
+//                     // add the triangle symbol
+//                     .attr("d", d3.symbol()
+//                             .type(d3.symbolTriangle)
+//                             .size(15))
+//                     .attr("transform", function (d) { 
+//                         return `translate(${x(d.x)},${y(d.y)})`})
+//                     .style("fill", "red");
+//     }
+//     return obj;
+// }
 
-// create a new chart and set the data
-var c = chart();
-c.createCanvas();
-c.setdata("csv/line.csv");
+// // create a new chart and set the data
+// var c = chart();
+// c.createCanvas();
+// c.setdata("csv/line.csv");
 
