5954ac8dd1bd04c7c8edabcdceadc4f2fe657b0c
kd41 <kyled073@gmail.com>
2022-02-16 22:05:47 +0000

    Ex 4

--- Lab2/js/Ex3.js
+++ Lab2/js/Ex3.js
@@ -1,26 +1,33 @@
-d3.select('body') 
-      .append('div') 
-      .style('width', '100px') 
-      .style('height', '20px') 
-      .style('background-color', 'green'); 
+// default values
+const width = 200;
+const height = 200;
+const radius = 10;
+
+var svg = d3.select("body")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .style("border", '1px solid green');
  
-    d3.selectAll("div") 
-      .on("mouseover", function(event){ 
-          d3.select(this) 
-            .style("background-color", "orange")
-            .style("border-style", "dotted")    // change style
-            .style("transform-origin", "top left")  // set origin then scale 2x
-            .style("transform", "scale(2)");                                                        6 
-          // Get current event info 
-          // Note: d3.event  (event) passed as the first argument to all listeners 
-          console.log(event); 
- 
-          // Get x & y co-ordinates 
-          // Note: d3.mouse was removed in d3v6, you should use d3.pointer(event) 
-          console.log(d3.pointer(event)); 
-      }) 
-      .on("mouseout", function(){ 
-          d3.select(this) 
-            .style("background-color", "steelblue") 
-            .style("border-style", "none");     // reset border style
-      }); 
+    svg.append("circle")
+        .attr("cx", width/2)
+        .attr("cy", height/2)
+        .attr("r", 10)
+        .attr("fill", "red")
+        .on("mouseover", function(event){ 
+            d3.select(this) 
+                // make circle larger
+                .attr("r", radius*10);                                                        6 
+                // Get current event info 
+                // Note: d3.event  (event) passed as the first argument to all listeners 
+                console.log(event); 
+    
+                // Get x & y co-ordinates 
+                // Note: d3.mouse was removed in d3v6, you should use d3.pointer(event) 
+                console.log(d3.pointer(event)); 
+        }) 
+        .on("mouseout", function(){ 
+            d3.select(this) 
+                // change its radius back to default
+                .attr("r", radius);   
+        }); 
