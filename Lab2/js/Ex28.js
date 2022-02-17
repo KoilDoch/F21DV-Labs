8dc0bf292f3232050e306f8f5989edcf2e2bc125
kd41 <kyled073@gmail.com>
2022-02-17 21:11:31 +0000

    Ex30

--- Lab2/js/Ex28.js
+++ Lab2/js/Ex28.js
@@ -7,15 +7,21 @@ d3.select('body').append('svg').attr('width',width).attr('height',height);
 // get data
 const dataset = d3.csv("Lab2/csv/years.csv");
 dataset.then((data) => {
+    var nodes = data.map((d) =>{
+        return {
+            year: +d.year,
+            val: Math.floor(d.value)
+        }});
+
     // create colours
-    var cols = d3.scaleOrdinal().domain([1,data.length]).range(d3.schemeSet1);
+    var cols = d3.scaleOrdinal().domain([1,nodes.length]).range(d3.schemeSet1);
 
     // create the simulation and set up options
-    var simulation = d3.forceSimulation(data) 
+    var simulation = d3.forceSimulation(nodes) 
     .force('charge', d3.forceManyBody().strength(5)) 
     .force('center', d3.forceCenter(width / 2, height / 2)) 
     .force('collision', d3.forceCollide().radius(function(d) { 
-        return d.value;     // updated from d.radius due to data set not containing that field
+        return d.val;     // updated from d.radius due to data set not containing that field
     })) 
     .on('tick', ticked); 
 
@@ -23,20 +29,35 @@ dataset.then((data) => {
     function ticked() { 
     var u = d3.select('svg') 
     .selectAll('circle') 
-    .data(data) 
+    .data(nodes) 
     .join('circle') 
     .attr('fill', (d,i) => cols(i))     // set colors according to index
     .attr('r', function(d) { 
-    return d.value; 
+    return d.val; 
     }) 
     .attr('cx', function(d) { 
     return d.x 
     }) 
     .attr('cy', function(d) { 
     return d.y 
-    }) 
+    })
+    .on("mouseover", displayValue) 
+    .on("mouseout", hideValue)
     } 
     console.log('ready..'); 
-
-
 });
+
+// displays the value when hovering over the bar
+function displayValue(d, i){
+    //display the value
+    d3.select("svg").append("text") 
+        .attr('class', 'val')  
+        .attr('x', () => i.x)
+        .attr('y', () => i.y)
+        .attr("font-size", "12px")
+        .text( function(d) { return i.val; } ); // Value of the text 
+}
+
+function hideValue(){
+    d3.selectAll(".val").remove()
+}
